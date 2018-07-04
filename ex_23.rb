5f08e71576f8eceefa6b959100fbc4fecfa9f207
Albert KW Wong <albertw1089@gmail.com>
2018-07-04 19:06:26 -0400

    (+) Exercise 23

---
+++ ex_23.rb
@@ -0,0 +1,4 @@
+# Exercise 23: Read Some Code
+
+# You should have spent the last week getting your list of symbols straight and locked in your mind.
+# Now you get to apply this to another week of reading code on the internet.
