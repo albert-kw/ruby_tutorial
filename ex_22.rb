ea8c8938cab8988208c7294244f55c5a80232cbe
Albert KW Wong <albertw1089@gmail.com>
2018-07-04 19:04:08 -0400

    (+) Exercise 22

---
+++ ex_22.rb
@@ -0,0 +1,5 @@
+# Exercise 22: What Do You Know So Far?
+
+# There won't be any code in this exercise, so there's no __What You Should See__
+# or Study Drills either. In fact, this exercise is like one giant Study Drills.
+# I'm going to have you do a review of what you have learned so far.
